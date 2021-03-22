00aefeee30e722928e011670b972219c463b53e3
marianaLz <marianaglp15@gmail.com>
2021-03-21 21:44:43 -0600

    Feature: delete subcategory filters

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,64 +1,31 @@
-import React, { useEffect, useState } from 'react';
-
-const sub1 = ['Vino tinto', 'Vino blanco', 'Espumantes'];
-const sub2 = ['Tequila', 'Ron', 'Grappa', 'Crema', 'Gin', 'Vodka', 'Whisky'];
-const sub3 = ['Chocolates', 'Salsas', 'Galletas', 'Conservas'];
-const sub4 = ['Agua natural', 'Agua carbonatada'];
+import React from 'react';
 
 const Filters = ({ onChange, arr, weights }) => {
-  const [path, setPath] = useState('');
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') setPath(window.location.pathname);
-  }, []);
-
-  const subcategory =
-    path === '/vinos'
-      ? sub1
-      : path === '/licores'
-      ? sub2
-      : path === '/gourmet'
-      ? sub3
-      : path === '/bebidas'
-      ? sub4
-      : [];
-
-  const data = [
-    {
-      name: 'SUBCATEGORÍA',
-      arr: subcategory,
-    },
-    {
-      name: 'TAMAÑO',
-      arr: weights,
-    },
-  ];
+  const data = {
+    name: 'TAMAÑO',
+    arr: weights,
+  };
 
   return (
     <div className="text-navbar px-5 mb-16">
       <p className="title">FILTROS</p>
-      {data.map(
-        (element, index) =>
-          element.arr.length !== 0 && (
-            <div className="mt-8" key={index}>
-              <p className="font-gotham-medium my-6">{element.name}</p>
-              <ul>
-                {element.arr.map((item, index) => (
-                  <li className="font-gotham-book my-3" key={index}>
-                    <input
-                      type="checkbox"
-                      name={item}
-                      checked={arr.includes(item)}
-                      className="checkbox mr-5"
-                      onChange={({ target }) => onChange(target)}
-                    />
-                    <span>{item}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )
-      )}
+      <div className="mt-8">
+        <p className="font-gotham-medium my-6">{data.name}</p>
+        <ul>
+          {data.arr.map((item, index) => (
+            <li className="font-gotham-book my-3" key={index}>
+              <input
+                type="checkbox"
+                name={item}
+                checked={arr.includes(item)}
+                className="checkbox mr-5"
+                onChange={({ target }) => onChange(target)}
+              />
+              <span>{item}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
